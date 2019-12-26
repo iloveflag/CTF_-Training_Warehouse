62a48ef35cf9da9ac12596b1131ce13c09598095
root <zmbxzrq@outlook.com>
2019-12-26 03:34:59 -0800

    add hash_hmac_php_bug

---
+++ hash_hmac_php_bug/html/index.php
@@ -0,0 +1,20 @@
+<?php
+highlight_file('index.php');
+if (empty($_POST['hmac']) || empty($_POST['host'])){
+	header('HTTP/1.0 400 Bad Request');
+	exit;
+}
+
+$secert = getenv('SECRET');
+
+if(isset($_POST['nonce']))
+	$secret=hash_hmac('sha256',$_POST['nonce'], $secret);
+
+$hmac = hash_hmac('sha256',$_POST["host"],$secret);
+
+if ($hmac !== $_POST['hmac']){
+	header('HTTP/1.0 403 Forbiden');
+	exit;
+}
+echo exec("host".$_POST['host']);
+?>
